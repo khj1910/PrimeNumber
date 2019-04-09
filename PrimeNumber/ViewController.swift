627a562aaec32c55dbd46cb8ebd595400368749b
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-04-09 15:48:09 +0900

    PrimeNumber

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -1,20 +1,65 @@
-//
-//  ViewController.swift
-//  PrimeNumber
-//
-//  Created by D7703_23 on 2019. 4. 9..
-//  Copyright © 2019년 D7703_23. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var LblA: UILabel!
+    @IBOutlet weak var textA: UITextField!
+    
+    @IBAction func Check(_ sender: Any) {
+        
+        // optional binding(Conditional unwraping)을 사용하여 숫자값을 제외함
+        if let number = Int(textA.text!) {
+            
+            var isPrime = true
+            
+            if number <= 0 {
+                LblA.text = "input correct value!"
+                textA.text = ""
+                return
+            }
+            
+            if number == 1 {
+                isPrime = false
+            }
+            
+            if number != 1 && number != 2 {
+                for i in 2 ..< number {
+                    if number % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                LblA.text = "prime number"
+            } else {
+                LblA.text = "Not Prime number"
+            }
+            textA.text = ""
+        } else {
+            //숫자값이 입력됐을때...
+            print("nil 발생")
+            LblA.text = "input numeric value!"
+            textA.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textA.delegate = self
+        textA.placeholder = "ex) 7"
+        textA.clearButtonMode = UITextField.ViewMode.always
     }
-
-
+    
+    // background view를 누르면 키패드가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // return 키를 누르면 키패드가 내려감
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textA.resignFirstResponder()
+        return true
+    }
 }
-
